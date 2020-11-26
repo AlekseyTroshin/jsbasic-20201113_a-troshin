403c521fb60a0b5f7a37102eae8751d29cc40a6a
aleksey <alekseyt@shtrafy-gibdd.ru>
2020-11-26 21:56:52 +0300

    3-module/4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,18 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  	let result = '';
+  	let firstUser = true;
+  	let lengthArray = users.length - 1;
+
+  	users.forEach((user, i) => {
+  		if(age >= user.age && firstUser) {
+  			result += `${user.name}, ${user.balance}`;
+  			firstUser = false;
+  		} else if(age >= user.age) {
+  			result += `\n${user.name}, ${user.balance}`;
+  		}
+  	});
+
+  	return result;
 }
